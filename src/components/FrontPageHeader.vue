7b6517056133741d7f805bb576e2c7f3015a2e7f
Viggo Petersen <vig56@hotmail.com>
2018-11-18 15:24:24 +0100

    Add front page with header and gallery component
    - add FrontPage view
    - add index route
    - add front page header component
    - add image gallery component with example data

---
+++ src/components/FrontPageHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="col-12 p-5">
+    <h1 class="m-4">{{ title }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class FrontPageHeader extends Vue {
+  @Prop({ required: true }) title: string;
+}
+</script>
---
+++ src/views/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <front-page-header
+      :title="title"
+    />
+    <image-gallery/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import FrontPageHeader from '@/components/FrontPageHeader.vue';
+import ImageGallery from '@/components/ImageGallery.vue'
+
+@Component({
+  components: {
+    FrontPageHeader,
+    ImageGallery
+  }
+})
+
+export default class FrontPage extends Vue {
+  title: string = 'Front page';
+}
+</script>
