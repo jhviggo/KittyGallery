01a34bf69698f9c09c469e3f3b30efa78f4d47b4
Viggo Petersen <vig56@hotmail.com>
2018-11-20 21:00:39 +0100

    Add commissions page
    - add hardcoded test data until CMS
    - add commissions form with inputs
    - add header with image
    - add commissions route
    - add empty commissions content file

---
+++ src/components/NetlifyForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <form class="input-group"
+        name="contact"
+        method="POST"
+        netlify-honeypot="bot-field"
+        netlify>
+    <div v-for="input in formInputs"
+         :key="input.name"
+         class="w-100 mb-4">
+      <input :name="input.name"
+             :type="input.type"
+             required="true"
+             class="form-control"
+             :id="`input-${input.name}`"
+             :placeholder="input.text"/>
+    </div>
+    <button class="btn btn-outline-success btn-block"
+            type="submit">
+      {{ formSubmitText }}
+    </button>
+  </form>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from 'vue-property-decorator';
+
+@Component
+export default class NetlifyForm extends Vue {
+  @Prop({ required: true }) formInputs!: Array<object>;
+  @Prop({ required: true }) formSubmitText!: string;
+}
+</script>
---
+++ src/views/commissions/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="mb-4 position-relative header__container">
+      <img class="w-100"
+           :src="headerImage"/>
+      <h1 class="header">{{ headerText }}</h1>
+    </div>
+    <div class="col-12 col-lg-6 mx-auto">
+      <netlify-form
+        :form-inputs="forms"
+        :form-submit-text="formText"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+import NetlifyForm from '@/components/NetlifyForm.vue';
+
+@Component({
+  components: {
+    NetlifyForm
+  },
+})
+
+export default class Commissions extends Vue {
+  page: Array<object> = require('@/content/commissions.json');
+  headerText: string = 'Contact me';
+  headerImage: string = '/img/commission_header.jpg';
+  formText: string = 'click me';
+  forms: Array<object> = [
+    {
+      name: 'name',
+      text: 'Name',
+      type: 'text'
+    },
+    {
+      name: 'email',
+      text: 'E-mail',
+      type: 'email'
+    },
+    {
+      name: 'phone',
+      text: 'Phone number',
+      type: 'tel'
+    },
+    {
+      name: 'about',
+      text: 'About',
+      type: 'text'
+    },
+
+  ];
+}
+</script>
+
+<style scoped>
+.header {
+  z-index: 50;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.header__container {
+  max-height: 150px;
+  overflow: hidden;
+}
+</style>
