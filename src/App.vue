104b9438e8c1af148d0c0df461b6050c9c7419ea
Viggo Petersen <vig56@hotmail.com>
2018-12-03 22:59:55 +0100

    Add mobile menu background
    - add opacity background
    - add noScroll when in mobile menu
    - add click to close on background

--- src/App.vue
+++ src/App.vue
@@ -45,6 +45,7 @@ export default class AppMain extends Vue {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  overflow-y: hidden;
 }
 
 .spacer {
--- src/components/TheNavigationBarMobile.vue
+++ src/components/TheNavigationBarMobile.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="nav-bar" id="mobileMenu">
-    <div class="d-flex justify-content-center" id="nav-bar__toggle" @click="toggleMenu">
-      <template v-if="menuOpen">X</template>
-      <template v-else>&#9776;</template>
-    </div>
-    <div class="icon-custom__container">
-      <router-link to="/">
-        <img class="w-50 h-100" :src="logo"/>
-      </router-link>
-    </div>
-    <div class="p-2 pl-3 btn-custom w-100 text-left" v-for="button in menuButtons" :key="button.url" @click="goToUrl(button.url)">
-      {{ button.text }}
+  <div>
+    <div id="nav-bar__background" @click="toggleMenu" v-if="menuOpen" class="w-100 h-100"></div>
+    <div class="nav-bar" id="mobileMenu">
+      <div class="d-flex justify-content-center" id="nav-bar__toggle" @click="toggleMenu">
+        <template v-if="menuOpen">X</template>
+        <template v-else>&#9776;</template>
+      </div>
+      <div class="icon-custom__container">
+        <div @click="goToUrl('/')">
+          <img class="w-50 h-100" :src="logo"/>
+        </div>
+      </div>
+      <div class="p-2 pl-3 btn-custom w-100 text-left" v-for="button in menuButtons" :key="button.url" @click="goToUrl(button.url)">
+        {{ button.text }}
+      </div>
     </div>
   </div>
 </template>
@@ -26,12 +29,19 @@ export default class TheNavigationBarMobile extends Vue {
   menuOpen: boolean = false;
 
   toggleMenu(): void {
+    if (!this.menuOpen) {
+      document.getElementById('app')!.style.maxHeight = `${document.getElementById('mobileMenu')!.offsetHeight}px`;
+    } else {
+      document.getElementById('app')!.style.maxHeight = 'none';
+    }
+
     document.getElementById('mobileMenu')!.classList!.toggle('nav-bar__display');
     this.menuOpen = !this.menuOpen;
   }
 
   goToUrl(url: string): void {
     this.$router.push(url);
+    this.toggleMenu();
   }
 }
 </script>
@@ -54,7 +64,7 @@ export default class TheNavigationBarMobile extends Vue {
 }
 
 .nav-bar {
-  transition: all 1s ease;
+  transition: all .5s ease;
   left: -60vw;
   width: 60vw;
   min-width: 150px;
@@ -62,6 +72,16 @@ export default class TheNavigationBarMobile extends Vue {
   background-color: #3C3C3C;
   position: fixed;
   z-index: 99;
+}
+
+#nav-bar__background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: #000;
+  z-index: 80;
+  opacity: .5;
+  filter: alpha(opacity=50);
 }
 
 #nav-bar__toggle {
