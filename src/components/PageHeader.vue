4197f2d408c057a9f5758ddbd99eb74381653d2d
Viggo Petersen <vig56@hotmail.com>
2018-12-04 19:51:47 +0100

    Add gallery header
    - add PageHeader component
    - add header to gallery page

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p class="mx-3">{{ text }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class PageHeader extends Vue {
+  @Prop({ required: true }) title!: string;
+  @Prop({ required: true }) text!: string;
+}
+</script>
--- src/views/gallery/index.vue
+++ src/views/gallery/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="margin-top-50">
+  <div>
+    <page-header
+      :title="title"
+      :text="text"
+    />
     <image-gallery/>
   </div>
 </template>
@@ -8,19 +12,19 @@
 import { Component, Vue } from 'vue-property-decorator';
 import FrontPageHeader from '@/components/FrontPageHeader.vue';
 import ImageGallery from '@/components/ImageGallery.vue'
+import PageHeader from '@/components/PageHeader.vue';
 
 @Component({
   components: {
     FrontPageHeader,
-    ImageGallery
+    ImageGallery,
+    PageHeader
   }
 })
 
-export default class Gallery extends Vue {}
+export default class Gallery extends Vue {
+  title: string = "My Gallery";
+  text: string = "Over the year i have worked on a lot og different pieces, below you can see most of them. Some of them were commission others were painted because i love painting";
+}
 </script>
 
-<style scoped>
-.margin-top-50 {
-  margin-top: 50px;
-}
-</style>
